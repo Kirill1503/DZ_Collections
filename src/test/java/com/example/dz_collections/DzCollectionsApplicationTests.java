28aaf145d0aec25d9380c203cc2b3fe521eee7c7
Kirill <K.katyshev1503@gmail.com>
2022-10-21 15:13:29 +0300

    Initial commit

---
+++ src/test/java/com/example/dz_collections/DzCollectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dz_collections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DzCollectionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
